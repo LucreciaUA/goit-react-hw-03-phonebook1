113121d03dbb41735814b8480af4011aa531a738
LucreciaUA <118127063+LucreciaUA@users.noreply.github.com>
2023-12-15 15:08:18 +0200

    2

--- src/components/phonebook.jsx
+++ src/components/phonebook.jsx
@@ -13,8 +13,7 @@ class Phonebook extends Component {
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ],
   filter: '',
-  name: '',
-  number: ''
+
     }
 
     generateId = () => {
